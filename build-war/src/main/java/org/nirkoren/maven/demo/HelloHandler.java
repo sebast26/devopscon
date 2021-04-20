1404d6ab2c6972c85909c267363ddad3c6f631df
Sebastian Gorecki <sebastian.gorecki@olx.pl>
2021-04-20 16:54:18 +0200

    It is working for me

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Dear participant, is it working from your laptop?";
+		String hello = "Dear participant, is it working from your laptop? It work for me ;-)";
 		return hello;
 	}
 
